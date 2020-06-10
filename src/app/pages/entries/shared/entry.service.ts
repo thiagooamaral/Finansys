d0395be44fd886652a909309744a250c38ac0a38
Thiago Amaral <tfmamaral@outlook.com>
2020-06-10 06:48:45 -0300

    adding EntryListComponent

--- src/app/pages/entries/shared/entry.service.ts
+++ src/app/pages/entries/shared/entry.service.ts
@@ -59,12 +59,17 @@ export class EntryService {
   //Private Methods
   private jsonDataToCategories(jsonData: any[]): Entry[] {
     const entries: Entry[] = [];
-    jsonData.forEach(element => entries.push(element as Entry));
+
+    // jsonData.forEach(element => entries.push(element as Entry)); element as: Faz apenas um cast, mas não cria na verdade um objeto tipado
+    jsonData.forEach(element => {
+      const entry = Object.assign(new Entry(), element);
+      entries.push(entry);
+    })
     return entries;
   }
 
   private jsonDataToEntry(jsonData: any): Entry {
-    return jsonData as Entry;
+    return Object.assign(new Entry(), jsonData);
   }
 
   private handleError(error: any): Observable<any> {
