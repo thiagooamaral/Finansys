b8807e072bff3767d2874bd9495ec545b3eb8243
Thiago Amaral <tfmamaral@outlook.com>
2020-06-18 17:42:09 -0300

    refactoring EntryFormComponent

--- src/app/pages/categories/category-form/category-form.component.ts
+++ src/app/pages/categories/category-form/category-form.component.ts
@@ -31,8 +31,8 @@ export class CategoryFormComponent extends BaseResourceFormComponent<Category> {
   }
 
   protected editionPageTitle(): string {
-    const categoryName = this.resource.name || '';
-    return 'Editando Categoria: ' + categoryName;
+    const resourceName = this.resource.name || '';
+    return 'Editando Categoria: ' + resourceName;
   }
 
 }
