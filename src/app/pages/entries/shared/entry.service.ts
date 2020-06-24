0d4a2cb921138de60048955032df40596fb41ce6
Thiago Amaral <tfmamaral@outlook.com>
2020-06-24 14:44:28 -0300

    improving Reports

--- src/app/pages/entries/shared/entry.service.ts
+++ src/app/pages/entries/shared/entry.service.ts
@@ -1,11 +1,13 @@
 import { Injectable, Injector } from '@angular/core';
 
 import { Observable } from "rxjs";
-import { flatMap, catchError } from "rxjs/operators";
+import { flatMap, catchError, map } from "rxjs/operators";
 
 import { BaseResourceService } from "../../../shared/services/base-resource.service";
 import { CategoryService } from "../../categories/shared/category.service";
 import { Entry } from "./entry.model";
+
+import * as moment from "moment";
 
 @Injectable({
   providedIn: 'root'
@@ -24,6 +26,12 @@ export class EntryService extends BaseResourceService<Entry> {
     return this.setCategoryAndSendToServer(entry, super.update.bind(this));
   }
 
+  getByMonthAndYear(month: number, year: number): Observable<Entry[]> {
+    return this.getAll().pipe(
+      map(entries => this.filterByMonthAndYear(entries, month, year))
+    )
+  }
+
   private setCategoryAndSendToServer(entry: Entry, sendFn: any): Observable<Entry> {
     //É utilizado este flatMap pois usamos o in-memory-database, para uma api de verdade, não é necessario
     return this.categoryService.getById(entry.categoryId).pipe(
@@ -33,6 +41,16 @@ export class EntryService extends BaseResourceService<Entry> {
       }),
       catchError(this.handleError)
     );
+  }
+
+  private filterByMonthAndYear(entries: Entry[], month: number, year: number) {
+    return entries.filter(entry => {
+      const entryDate = moment(entry.date, 'DD/MM/YYYY');
+      const monthMatches = entryDate.month() + 1 == month;
+      const yearMatches = entryDate.year() + 1 == year;
+
+      if (monthMatches && yearMatches) return entry;
+    })
   }
 
 }
